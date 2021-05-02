a0b2f2690fea7fccd27252060e4ae70a7634763e
卢见信 <lujianxin@lujianxindeMacBook-Pro.local>
2021-05-02 14:20:18 +0800

    绑定云服务空间

---
+++ pages/about/about.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		},
+		async onLoad() {
+			const res = await uniCloud.callFunction({
+				name: 'login'
+			})
+			console.log(res)
+		}
+	}
+</script>
+
+<style>
+
+</style>
